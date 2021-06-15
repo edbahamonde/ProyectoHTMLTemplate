8c3287d2f81ffb733f05998f2e66bff08d8250fb
edbahamonde <49206926+edbahamonde@users.noreply.github.com>
2021-06-14 19:00:20 -0500

    Unir 2
    Unir 2

--- Descripcion.js
+++ Descripcion.js
@@ -27,3 +27,19 @@ class Caracteristicas extends HTMLElement{
     }
   }
   window.customElements.define('miguel-castillo', tiposVehiculo);
+
+  //Dayan
+  class Header extends HTMLElement{
+    constructor(){
+      super();
+    }
+    
+    connectedCallback(){
+      let shadowRoot = this.attachShadow({mode: 'open'});
+      const tem = document.querySelector('#header');
+      const instance = tem.content.cloneNode(true);
+      shadowRoot.appendChild(instance);
+    }
+  }
+  
+  window.customElements.define('section-header', Header);
