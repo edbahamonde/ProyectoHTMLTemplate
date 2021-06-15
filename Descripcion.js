15934d5a73fc874573ad0b9c25b2f1494cd60ef0
edbahamonde <49206926+edbahamonde@users.noreply.github.com>
2021-06-14 20:04:53 -0500

    Corrección de faltas ortográficas.
    Se corrigieron faltas ortográficas.

--- Descripcion.js
+++ Descripcion.js
@@ -43,3 +43,20 @@ class Caracteristicas extends HTMLElement{
   }
   
   window.customElements.define('section-header', Header);
+
+  //Yomar
+
+  class Footer extends HTMLElement{
+    constructor(){
+      super();
+    }
+    
+    connectedCallback(){
+      let shadowRoot = this.attachShadow({mode: 'open'});
+      const tem = document.querySelector('#Footer');
+      const instance = tem.content.cloneNode(true);
+      shadowRoot.appendChild(instance);
+    }
+  }
+  
+  window.customElements.define('section-footer', Footer);
