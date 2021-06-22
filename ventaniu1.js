7223ec85ce73c8553663dd7c5c85eb8d17b6b2d8
YomarPeralta1603 <yomarpt1603@gmail.com>
2021-06-22 12:44:18 -0500

    acerca

---
+++ ventaniu1.js
@@ -0,0 +1,166 @@
+class CajaContenedor extends HTMLElement{
+    constructor(){
+      super();
+    }
+    connectedCallback(){
+      let shadowRoot = this.attachShadow({mode:'open'}); // es importante el modo en close no aparece en el HTML
+     
+      //aqui se crea el botton y se da estilos al boton encapsulado dentro del shutdown DOM
+      shadowRoot.innerHTML =  `
+      <style>
+        .box, .box2{
+            width: 300px;
+            height: 200px;
+            color: rgb(214, 214, 214);  
+            background-color: rgb(0, 0, 20); 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            font-size: 2rem;
+          
+            font-family: 'Montserrat', sans-serif;
+            transition: transform 1s;
+        }
+        .box:hover{
+            font-size: 0;
+            color: #2abb9b;
+            transform: scale(1);
+            display:block;
+            background-image: url(img/somos.jpg); 
+            background-size: 100% 100%;
+            font-family: 'Montserrat', sans-serif;
+            text-align: bottom;
+            margin-top: 2px;
+            line-height: 100px;
+            vertical-align: text-bottom;
+           
+         
+        }
+        .box2:hover{
+            font-size: 0;
+            color: #2abb9b;
+            transform: scale(1);
+            display:block;
+            background-image: url(img/mision.jpg); 
+            background-size: 100% 100%;
+            font-family: 'Montserrat', sans-serif;
+            text-align: bottom;
+            margin-top: 2px;
+            line-height: 100px;
+            vertical-align: text-bottom;
+           
+         
+        }
+    
+        .box, .box2{
+            width: 300px;
+            height: 200px;
+            color: rgb(214, 214, 214);  
+            background-color: rgb(0, 0, 20); 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            font-size: 2rem;
+          
+            font-family: 'Montserrat', sans-serif;
+            transition: transform 1s;
+        }
+        .box:hover{
+            font-size: 0;
+            color: #2abb9b;
+            transform: scale(1);
+            display:block;
+            background-image: url(img/somos.jpg); 
+            background-size: 100% 100%;
+            font-family: 'Montserrat', sans-serif;
+            text-align: bottom;
+            margin-top: 2px;
+            line-height: 100px;
+            vertical-align: text-bottom;
+           
+         
+        }
+        .box2:hover{
+            font-size: 0;
+            color: #2abb9b;
+            transform: scale(1);
+            display:block;
+            background-image: url(img/vision.jpeg); 
+            background-size: 100% 100%;
+            font-family: 'Montserrat', sans-serif;
+            text-align: bottom;
+            margin-top: 2px;
+            line-height: 100px;
+            vertical-align: text-bottom;
+           
+         
+        }
+        </style> 
+        <div class="box">
+        MAZDA - EC <br> 
+        ¡Tu mejor opcion!
+         </div>
+         <div class="box2">
+         MAZDA - EC <br> 
+         ¡Tu mejor opcion!
+         </div>
+        `;
+  
+    }
+  }
+  window.customElements.define('caja-container', CajaContenedor);
+
+
+  class CajaContenedor2 extends HTMLElement{
+    constructor(){
+      super();
+    }
+    connectedCallback(){
+      let shadowRoot2 = this.attachShadow({mode:'open'}); // es importante el modo en close no aparece en el HTML
+     
+      //aqui se crea el botton y se da estilos al boton encapsulado dentro del shutdown DOM
+      shadowRoot2.innerHTML =  `
+      <style>
+        .box2{
+            width: 300px;
+            height: 200px;
+            color: rgb(214, 214, 214);  
+            background-color: rgb(0, 0, 20); 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            font-size: 2rem;
+          
+            font-family: 'Montserrat', sans-serif;
+            transition: transform 1s;
+        }
+        .box2:hover{
+            font-size: 0;
+            color: #2abb9b;
+            transform: scale(1);
+            display:block;
+            background-image: url(img/mision.jpg); 
+            background-size: 100% 100%;
+            font-family: 'Montserrat', sans-serif;
+            text-align: bottom;
+            margin-top: 2px;
+            line-height: 100px;
+            vertical-align: text-bottom;
+           
+         
+        }
+    
+    
+        </style> 
+         <div class="box2">
+           MAZDA - EC <br> 
+            ¡Tu mejor opcion!
+         </div>
+        `;
+  
+    }
+  }
+  window.customElements.define('caja-container2', CajaContenedor2);
